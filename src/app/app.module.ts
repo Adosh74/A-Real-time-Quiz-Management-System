8e038911cc58fe55a0683bfa50029811b8bfa1df
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2022-12-10 22:23:37 +0200

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { SenderComponent } from './sender/sender.component';
 import { RecieveComponent } from './recieve/recieve.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideAuth, getAuth } from '@angular/fire/auth';
 import { ServerService } from './services/server.service';
 import { AuthService } from './services/auth.service';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 const appRoutes: Routes = [
-  { path: "", component: SenderComponent },
-  { path: "recieve", component: RecieveComponent },
-  { path: "register", component: RegisterComponent },
-  { path: "login", component: LoginComponent },
- 
+  { path: '', component: SenderComponent },
+  { path: 'recieve', component: RecieveComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
@@ -37,9 +36,9 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideFirestore(() => getFirestore()),
-    provideAuth(() => getAuth())
+    provideAuth(() => getAuth()),
   ],
   providers: [ServerService, AuthService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
