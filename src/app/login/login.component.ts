4240d494e43f211ba79ffceba621123f4eb69548
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2022-12-20 14:54:30 +0200

    get user id

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,22 +8,23 @@ import { AuthService } from '../services/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  @ViewChild('f') loginForm?: NgForm;
 
-  @ViewChild("f") loginForm?: NgForm;
-
-  constructor(private authService: AuthService,
-    private router: Router) { }
+  constructor(private authService: AuthService, private router: Router) {}
 
   onSubmit() {
-    let user = new UserAuth(this.loginForm?.value.username, this.loginForm?.value.password)
-    
+    let user = new UserAuth(
+      this.loginForm?.value.username,
+      this.loginForm?.value.password
+    );
+
     this.authService.login(user).then((userData: UserCredential) => {
       console.log(userData.user.uid);
+      this.authService.setid(userData.user.uid);
       this.router.navigate(['/']);
     });
   }
-
 }
